e56c8e2b7785061cb5ca38882e332e0f844af9e0
Gitansh Shekhar <gitanshshekhar2541@gmail.com>
2025-08-16 19:15:37 +0530

    function part worked started

---
+++ script.js
@@ -0,0 +1,5 @@
+const accesskey="66sSAXhA1tjZXGS_3I4oIHwpw2I-Trf5wVI1_0ixkgw"
+const formEl=document.querySelector("form");
+const searchInputEl=document.getElementById("search-input");
+const searchresultEl=document.querySelector(".search-results")
+const showMoreButton=document.getElementById("show-more-button");
